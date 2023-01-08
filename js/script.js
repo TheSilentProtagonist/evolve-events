d99aa8f4d269140d56bcd904b96fb544c2e6458e
Maria Konova <arim.novak@gmail.com>
2023-01-08 21:34:30 +0000

    adds a map with event markers

--- js/script.js
+++ js/script.js
@@ -1,25 +1,44 @@
 // Getting API details
-var apiKey = 'A3GWr1bAKM9umGLc1IVcH5AWbfGcFkxQ';
-var apiURL = 'https://app.ticketmaster.com/discovery/v2/events.json?sort=random&countryCode=GB&apikey=';
-var proxyURL = 'https://api.allorigins.win/get?url=';
-
-
-
+var apiKey = "A3GWr1bAKM9umGLc1IVcH5AWbfGcFkxQ";
+var googleAPI = "AIzaSyC9JbnPQ6SRFQ6BdafXmHHL2E8DGPnyiRM";
+var apiURL =
+  "https://app.ticketmaster.com/discovery/v2/events.json?sort=random&countryCode=GB&apikey=";
+var proxyURL = "https://api.allorigins.win/get?url=";
 
 // Targeting html elements and store to variables
-var searchInput = $('.search');
-var itemWrapper = $('main');
-var exitButton = $('#reset');
+var searchInput = $(".search");
+var itemWrapper = $("main");
+var exitButton = $("#reset");
+var map;
+// creates a map
+function initMap() {
+  var mapDiv = document.getElementById("map");
+  map = new google.maps.Map(mapDiv, {
+    center: { lat: 51.509865, lng: -0.118092 },
+    zoom: 10,
+  });
+}
 
+// adds markers for events
+function addMarker(map, event) {
+  var marker = new google.maps.Marker({
+    position: new google.maps.LatLng(
+      event._embedded.venues[0].location.latitude,
+      event._embedded.venues[0].location.longitude
+    ),
+    map: map,
+  });
+}
 
 // Function to create HTML div (use backtick so you can insert variables) to display events for an inputted city
 function displayMatches(matches) {
-  itemWrapper.html('');  // clear off the paragraph text when a city is entered
+  itemWrapper.html(""); // clear off the paragraph text when a city is entered
 
   if (!matches) {
-    itemWrapper.html(`<p class="no-search">No results found.</p>`);  // Displaying no results found if city entered is not found. 
+    itemWrapper.html(`<p class="no-search">No results found.</p>`); // Displaying no results found if city entered is not found.
   } else {
-    for (var matchObj of matches) {                              // Loop through the the events for a city and display them on the screen with their titles, date, time, venue and background image as shown in the var=events (console.log(events) to see the various properties to pass in to view title and the rest). Styling of the background image is done in CSS
+    for (var matchObj of matches) {
+      // Loop through the the events for a city and display them on the screen with their titles, date, time, venue and background image as shown in the var=events (console.log(events) to see the various properties to pass in to view title and the rest). Styling of the background image is done in CSS
       itemWrapper.append(`         
               <div class="event-item" style="background-image:
               linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),
@@ -28,42 +47,36 @@ function displayMatches(matches) {
               <p><b>Event Date</b>: ${matchObj.dates.start.localDate} </p>
               <p><b>Event Time</b>: ${matchObj.dates.start.localTime} </p>
               <p><b>Venue Name</b>: ${matchObj._embedded.venues[0].name} </p>
-              <p><b>City</b>: ${matchObj._embedded.venues[0].city.name} </p>
-              <p><b>Country</b>: ${matchObj._embedded.venues[0].country.name} </p>
-              <p><b>Postal Code</b>: ${matchObj._embedded.venues[0].postalCode} </p>
               <a href=${matchObj.url} target="_blank">View More Info About Event</a>
-               
               </div> 
             `);
+      addMarker(map, matchObj);
     }
   }
-
-};
-
-
+}
 
 // Function to get events for city name entered by user.
 function getEventData(event) {
-  var keyCode = event.keyCode;   // Get the key that was pressed
-  var searchText = searchInput.val().trim();  // Gets the input entered by user and remove any spacings using the trim(). (the database is not case sensitive so no need to use the toLowerCase() 
+  var keyCode = event.keyCode; // Get the key that was pressed
+  var searchText = searchInput.val().trim(); // Gets the input entered by user and remove any spacings using the trim(). (the database is not case sensitive so no need to use the toLowerCase()
 
-  if (keyCode === 13 && searchText) {    // Checking to see if the key pressed is the enter key and if some text is typed in the input box
+  if (keyCode === 13 && searchText) {
+    // Checking to see if the key pressed is the enter key and if some text is typed in the input box
 
     // Making an AJAX Request to get data from a server using the Ticketmaster API. The proxyURL and encodeURIComponent() function takes care of the CORS issue in the browser
-    $.get(proxyURL + encodeURIComponent(apiURL + apiKey + `&city=${searchText}`))
-      .then(function (data) {
-        var events = JSON.parse(data.contents)._embedded.events;
-        console.log(events);
-        displayMatches(events);
-      }); // fetching events from external server based on the city the user types in - the .get() method in jQuery makes this so easy it's like the fetch method in vanilla javaScript but it does all the json and parsing for us
-
+    $.get(
+      proxyURL + encodeURIComponent(apiURL + apiKey + `&city=${searchText}`)
+    ).then(function (data) {
+      var events = JSON.parse(data.contents)._embedded.events;
+      console.log(events);
+      displayMatches(events);
+    }); // fetching events from external server based on the city the user types in - the .get() method in jQuery makes this so easy it's like the fetch method in vanilla javaScript but it does all the json and parsing for us
 
     /* // Shorter and modern version of the code for making AJAX request in vanilla javaScript to a server (Arrow Function)
     fetch('https://www.omdbapi.com/?apikey=2a194df&t=drive')
     .then(res => res.json())
     .then(data => console.log(data));*/
   }
-
 }
 
 // Function to show more details if the 'view more details' link is clicked on a movie
@@ -101,9 +114,6 @@ function init() {
           showMovieDetails(anchorLink.dataset.id)
       }
   });*/
-
 }
 
 init();
-
-
